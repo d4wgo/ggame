3a4212a8577fe9325df431311a559c8feaa15924
doggo251 <37166660+doggo251@users.noreply.github.com>
2019-01-24 20:09:33 -0800

    e

--- static/signin.js
+++ static/signin.js
@@ -5,8 +5,15 @@ function onSignIn(googleUser) {
     proUrl = profile.getImageUrl();
     document.getElementById("profPic").src = proUrl;
     document.getElementById("mainSignIn").style.visibility = "hidden";
+    document.getElementById("signOut").style.visibility = "visible";
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
 }
